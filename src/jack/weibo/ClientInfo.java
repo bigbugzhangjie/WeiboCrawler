5d5c8b332dd587d923cceafee5c430c67b3b9c1a
bigbugzhang <bigbugzhang@126.com>
2014-09-03 14:12:07 +0800

    .

--- src/jack/weibo/ClientInfo.java
+++ src/jack/weibo/ClientInfo.java
@@ -1,23 +1,31 @@
 package jack.weibo;
 
 public class ClientInfo {
-	public static final ClientInfo CLIENT= new ClientInfo("1955726735","0a5232ffbfcd7d6eab4ee73b965dfbc1","http://127.0.0.1:8080");
+	public static final ClientInfo CLIENT = new ClientInfo(
+			"4062999603",
+			"dfb9e861370509ebddd75832f238b00b",
+			"http://127.0.0.1:8080");
 
 	String id;
 	String sercret;
 	String redirectURI;
+	
 	public ClientInfo(){}
+	
 	public ClientInfo(String id, String secr, String uri){
 		this.id = id;
 		this.sercret = secr;
 		this.redirectURI = uri;
 	}
+	
 	public String getId() {
 		return id;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+	
 	public String getSercret() {
 		return sercret;
 	}
